74fa7fbf04ae14c6492c9c606128923484677822
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-02-21 17:05:31 -0600

    Add files via upload

---
+++ Clicker Game.lua
@@ -0,0 +1,15 @@
+local dlg = Dialog("Clicker Game")
+local m = 0
+local n = 1
+
+while(n == 1)
+do
+    local result = app.alert{ title="Clicker Game", text="You have " .. m .. " clicks!", buttons={"Click", "Info", "Exit"}}
+    if result == 1 then
+        m = m + 1
+    elseif result == 2 then
+        app.alert{ title="Info", text="Clicker version 1.1 by MrEnder", buttons={"Ok"}}
+    else
+        n = 0
+    end
+end
