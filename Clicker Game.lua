c831ee8548fa58933a650d289be998a40e793e8c
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-02-21 18:51:58 -0600

    Cleaned up code

--- Clicker Game.lua
+++ Clicker Game.lua
@@ -1,15 +1,14 @@
-local dlg = Dialog("Clicker Game")
-local m = 0
-local n = 1
+local m=0
+local n=1
 
 while(n == 1)
 do
     local result = app.alert{ title="Clicker Game", text="You have " .. m .. " clicks!", buttons={"Click", "Info", "Exit"}}
     if result == 1 then
-        m = m + 1
+        m=m+1
     elseif result == 2 then
-        app.alert{ title="Info", text="Clicker version 1.1 by MrEnder", buttons={"Ok"}}
+        app.alert{ title="Info", text="Clicker version 1.1 by MrEnder", buttons={"Ok"} }
     else
-        n = 0
+        n=0
     end
 end
